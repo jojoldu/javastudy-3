48118c321c62a2cae1512b29f15b62489d23150a
이동욱 <jojoldu@zuminternet.com>
2015-03-10 16:39:49 +0900

    2주차 숙제 2번 추가

---
+++ src/homework/week2/Test2.java
@@ -0,0 +1,49 @@
+package homework.week2;
+
+import java.util.Scanner;
+
+public class Test2 {
+
+	public static void main(String[] args){
+
+		int num,n,i,j,k;
+
+		Scanner input = new Scanner(System.in);
+		System.out.print("크기를 입력하세요 : ");
+		num=input.nextInt();
+
+		int array[][]= new int[num][num];
+
+		int element =1;
+		j=0;
+
+		for(n=num; n>0;n-=2){
+			for(i=0;i<n;i++){
+				array[j][j+i]=element;
+				element++;
+
+			}
+			for(k=1;k<n;k++){
+				array[j+k][num-j-1]=element;
+				element++;
+			}
+			for(i=1;i<n;i++){
+				array[j+k-1][num-1-j-i]=element;
+				element++;
+			}
+			for(k=1;k<n-1;k++){
+				array[num-1-j-k][j]=element;
+				element++;
+			}
+
+			j++;
+		}
+
+		for(i=0;i<num;i++){
+			for(j=0;j<num;j++){
+				System.out.printf("%3d",array[i][j]);
+			}
+			System.out.println("");
+		}
+	}
+}
