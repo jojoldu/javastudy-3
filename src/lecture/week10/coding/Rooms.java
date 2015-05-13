f06e62e88ed415dab183e293061696ee68a82b9e
이동욱 <jojoldu@zuminternet.com>
2015-05-13 20:29:20 +0900

    rooms 코드 수정

--- src/lecture/week10/coding/Rooms.java
+++ src/lecture/week10/coding/Rooms.java
@@ -11,37 +11,45 @@ public class Rooms {
 										{true,	true,	true,	true,	true}
 									};	
 	
+	private static boolean[][] newRooms = new boolean[MAX_H+1][MAX_W+1];
 	
 	public static boolean dfs(int height, int width){
+		if(height == 0 && width == 0){
+			for(int i=0;i<MAX_H; i++){
+				for(int j=0;j<MAX_W;j++){
+					newRooms[i][j] = rooms[i][j];
+				}
+			}
+		}
 		
-		rooms[height][width] = false;
-		
+		newRooms[height][width] = false;
 		System.out.println(height + ": " + width);
 		
 		if(height == MAX_H-1 && width == MAX_W-1){
 			System.out.println("성공");
 			return true;
 		}
-		else if(width > 0 && rooms[height][width-1] && dfs(height, width-1)){
+		else if(width > 0 && newRooms[height][width-1] && dfs(height, width-1)){
 			return true;
 		}
-		else if(width < MAX_W && rooms[height][width+1] && dfs(height, width+1)){
+		else if(width < MAX_W && newRooms[height][width+1] && dfs(height, width+1)){
 			return true;
 		}
-		else if(height > 0 && rooms[height-1][width] && dfs(height-1, width)){
+		else if(height > 0 && newRooms[height-1][width] && dfs(height-1, width)){
 			return true;
 		}
-		else if(height < MAX_H && rooms[height+1][width] && dfs(height+1, width)){
+		else if(height < MAX_H && newRooms[height+1][width] && dfs(height+1, width)){
 			return true;
 		}
 		else{
-			rooms[height][width] = true;
+			newRooms[height][width] = true;
 			return false;
 		}
 	}
 	
 	
 	public static void main(String[] args) {
+		
 		System.out.println(dfs(0,0));
 	}
 }
